9fd26817e84eb11b330d8e64a89bba20b566064f
xanoka <rutecoelho86@gmail.com>
2021-02-07 15:56:06 +0000

    Clean Project

--- src/test_ZePedro.java
+++
@@ -1,8 +0,0 @@
-public class test_ZePedro {
-    public test_ZePedro () {
-        System.out.println("I'm Jose Pedro.");
-        System.out.println("I'm Jose Pedro.");
-        System.out.println("I'm Jose Pedro.");
-
-    }
-}
