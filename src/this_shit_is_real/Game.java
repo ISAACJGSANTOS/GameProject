ddb1c5faa58933eda7441c9c69c0f58c3e5b192a
josepfpinto <josepfpinto@gmail.com>
2021-02-11 12:33:46 +0000

    StartMenu working - buttons, start and quit working.

--- src/this_shit_is_real/Game.java
+++ src/this_shit_is_real/Game.java
@@ -17,20 +17,32 @@ public class Game {
 
     public void init() {
         gamePlay = new GamePlay(this);
-        GameObjectsFactory factory = new GameObjectsFactory(field, gamePlay);
-        // startMenu = new StartMenu(factory,this, getField());
 
+        GameObjectsFactory factory = new GameObjectsFactory(field, gamePlay);
+
+        startMenu = new StartMenu(factory,this, getField());
+
+        while (startMenu.isMenuOn()) {
+            System.out.println("");
+        }
+        if (startMenu.endGame()) { quit(); }
+
+        startMenu.deleteMenu();
         play();
     }
 
     public void play() {
+
         gamePlay.init();
         Wait.wait(500);
         gamePlay.start();
     }
 
+    public void quit() {
+        System.exit(0);
+    }
+
     public Field getField() {
         return field;
     }
-
 }
